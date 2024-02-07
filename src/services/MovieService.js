d6a12d838afaed08df70631c4b02490e04abf8f6
Supawitkaennak66 <supawit_ka661@live.rmutl.ac.th>
2024-02-07 17:02:42 +0700

    fix 7/2/24

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -3,7 +3,7 @@ const bearerToken = '1234567890';
 export async function getAllMovies(bearerToken) {
     try {
         const response = await fetch('https://api.se-rmutl.net/api/movie/all', {
-            headers: {Authorization: `Bearer 1234567890`}
+            headers: {Authorization: `Bearer ${bearerToken}`}
         });
         return await response.json();
     } catch (error) {
@@ -17,7 +17,7 @@ export async function createMovie(data, bearerToken) {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                Authorization: `Bearer 1234567890`
+                Authorization: `Bearer ${bearerToken}`
             },
             body: JSON.stringify(data)
         });
@@ -31,7 +31,7 @@ export async function getMovieSearch(search_text, bearerToken) {
     try {
         const response = await fetch(`https://api.se-rmutl.net/api/movie/search?search_text=${search_text}`, {
             headers: {
-                Authorization: `Bearer 1234567890`
+                Authorization: `Bearer ${bearerToken}`
             }
         });
         return await response.json();
